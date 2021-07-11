3fbf43cbc3e90b06222abf22fbd8eb6654e4fb5b
Hann1234 <christopherjhannigan@gmail.com>
2021-07-11 10:33:46 -0500

    modified tge gallery.data.js file to include pics

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/conan.jpg', description: 'An evening filled with lots of laughs seeing my favorite comedian, Conan Obrien.', likes: 0 },
+    { id: 2, path: 'images/jackWhite.jpg', description: 'What an amazing concert being able to see the living legend, Jack White, live at Boston Calling!', likes: 0 },
+    { id: 3, path: 'images/lobsterRoll.jpg', description: 'Award winning best lobster roll in all of Boston made the right way at the hole in the wall seafood counter, Alive & Kicking Lobsters.', likes: 0 },
+    { id: 4, path: 'images/whistler.jpg', description: 'High atop the Blackcomb Mountain on our way to the Blackcomb Glacier backbowl at Whistler, Canada. Send it!', likes: 0 },
+    { id: 5, path: 'images/islandLife.jpg', description: 'A calm evening on the beaches of Punta Cana, Dominican Republic. Thankful I was able to travel there, experience the culture, and celebrate my friends wedding with them.', likes: 0 },
+    { id: 6, path: 'images/tree.jpg', description: 'Getting out and exploring nature was an important respite from lockdown. This was near Pepin, WI on the Mississippi River.', likes: 0 }
 ];
 
 module.exports = galleryItems;
